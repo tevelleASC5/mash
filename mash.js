bfdcac08ffb93e570576ced71ade9c3bf0bb400b
tevelleASC5 <41081590+tevelleASC5@users.noreply.github.com>
2018-07-15 11:31:00 -0400

    Update mash.js

--- mash.js
+++ mash.js
@@ -1,48 +1,73 @@
+let bannyboys=["Joel", "Jaden", "Said", "Charles", "Kaba", "DiMarcus", "Khalid"];
 
-
-////////////////            1          ///////////////////
-let homes = ["Mansion", "Apartment", "Shack", "House", "box on the street", process.argv[2]];
-function getHome(somehomes){
-    let homes2 = Math.random()*somehomes.length;
-    let homes3 = Math.floor(homes2);
-    let homes4 = somehomes[homes3];
-    console.log(process.argv[2]);
-    return homes4;
+function lovegenerator(){
+let names = Math.random()*bannyboys.length;
+let names2 = Math.floor(names);
+let finalname= bannyboys[names2];
+//console.log(finalname);
+return finalname
 }
-getHome(homes);
+lovegenerator();
 
-/////////////////             2             /////////////////
-function getChildrenCount(){
-        const decimal = Math.random()
-        const times100 = decimal * 100
-        const five = times100 * .50
-        const final = Math.floor(five);
-        if(Math.random() > 0.5){
-            return process.argv[3];
-        }else{
-        return final;
-        }
+
+
+let bannygirls=["Savika", "Tenae", "Kiara", "Nia", "Sanaa", "Tay Tay Banks", "Mikaylah"];
+
+function lovegenerator2(){
+let names1 = Math.random()*bannygirls.length;
+let names3 = Math.floor(names1);
+let finalname2= bannygirls[names3];
+//console.log(finalname2);
+return finalname2
 }
-
-getChildrenCount();
-
-///////////////////////        3         ///////////////////
+lovegenerator2();
 
 
-///////////////////////        4          /////////////////////
-let cars = ["Jeep", "Nissan", "...SIKE!! You walk bro", "car owned by your mom", "church van", process.argv[2]];
-function getCar(somecars){
-    let cars2 = Math.random()*somecars.length;
-    let cars3 = Math.floor(cars2);
-    let cars4 = somecars[cars3];
-    console.log(process.argv[3]);
-    return cars4;
+
+let cars=["Jeep", "...SIKE! You walk", "Shopping Cart", "Lambo", "BMW", "Honda Accord", "Nissan"];
+
+function carz(){
+    let cars1 = Math.random()*cars.length;
+    let cars3 = Math.floor(cars1);
+    let finalcars= cars[cars3];
+   // console.log(finalcars);
+    return finalcars
     }
-getHome(cars);
+    carz();
 
 
 
-function mash(){
-    console.log("You will live in a " + getHome(homes) + " , and you will have " + getChildrenCount() + " kids!, and you will drive a " + getCar(cars));
-}
-mash();
+let homes=["box on the street", "townhouse", "mansion", "shelter", "store", "hospital", "condo"];
+function homez(){
+        let homes1 = Math.random()*homes.length;
+        let homes3 = Math.floor(homes1);
+        let finalhomes= homes[homes3];
+        //console.log(finalhomes);
+        return finalhomes
+        }
+        homez();
+
+
+
+function kidz(){
+         const decimal = Math.random()
+         const times50 = decimal * 50
+         const final = Math.floor(times50)
+        // console.log(final);
+        return final
+        }
+        kidz();
+
+
+
+let salary=["$1,000,000", "$1", "just enough money to survive", "$50,000", "...You wont have any income so $0", "$56.78", "$500,000"];
+function moneyz(){
+        let money1 = Math.random()*salary.length;
+        let money3 = Math.floor(money1);
+        let finalmoney= salary[money3];
+       // console.log(finalmoney);
+        return finalmoney
+        }
+        moneyz();
+
+        console.log("You will marry " + lovegenerator2() + ", you will drive a " + carz() + ", you will live in a " + homez() + ", you guys will have " + kidz() + " kids, and you will make " +  moneyz() + " a year!"); 
